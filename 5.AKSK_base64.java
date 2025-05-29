740b6cee5d95961700064d4c7b88256c09e3fc3c
beyond-sec <beyondsec@163.com>
2025-05-29 15:02:27 +0800

    1

---
+++ 5.AKSK_base64.java
@@ -0,0 +1,19 @@
+text-aksk
+这是测试的aksk，无风险，泄漏不会有风险。。
+
+base64编码
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!\n");
+        String AccessId= "TFRBSTV0RXQyYzhCeTloUTdwOXFCS3JR";
+        String AccessKey= "MU52aWhMNG1kZkw5cGlSOWlxTlRWQ1FXclhVRU9l";
+        if(AccessId !=null && AccessKey != null)
+        {
+            System.out.println(AccessId);
+            System.out.println(AccessKey);
+        }
+    }
+}
+
+accessID=TFRBSTV0RXQyYzhCeTloUTdwOXFCS3JR
+accesskey=MU52aWhMNG1kZkw5cGlSOWlxTlRWQ1FXclhVRU9l
